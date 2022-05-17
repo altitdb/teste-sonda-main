cee21157ae8d11d97dd2863ad598bf2182c195e7
Altieres de Matos <almatos@uolinc.com>
2022-05-16 23:01:01 -0300

    Remove unnecessary annotation

--- src/main/java/br/com/elo7/sonda/candidato/controller/PlanetAndProbeController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controller/PlanetAndProbeController.java
@@ -7,19 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.elo7.sonda.candidato.dto.InputDTO;
 import br.com.elo7.sonda.candidato.model.Probe;
 import br.com.elo7.sonda.candidato.service.ProbeService;
 
 @Controller
-@RequestMapping("/planet-with-probes")
 public class PlanetAndProbeController {
 	@Autowired
 	private ProbeService probeService;
 
-	@PostMapping
+	@PostMapping("/planet-with-probes")
     public ResponseEntity<List<Probe>> register(@RequestBody InputDTO inputDto) {
 		return ResponseEntity.ok(probeService.landProbes(inputDto));        
     }
