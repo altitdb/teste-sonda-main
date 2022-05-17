e677ebaf4743af43747c99a7c671b9c6cd84cfc2
Altieres de Matos <almatos@uolinc.com>
2022-05-17 00:48:40 -0300

    Transform class in record

--- src/main/java/br/com/elo7/sonda/candidato/dto/ProbesResponseDTO.java
+++ src/main/java/br/com/elo7/sonda/candidato/dto/ProbesResponseDTO.java
@@ -2,13 +2,5 @@ package br.com.elo7.sonda.candidato.dto;
 
 import java.util.List;
 
-public class ProbesResponseDTO {
-    private final List<ProbeResponseDTO> probes;
-    public ProbesResponseDTO(List<ProbeResponseDTO> probes) {
-        this.probes = probes;
-    }
-
-    public List<ProbeResponseDTO> getProbes() {
-        return this.probes;
-    }
+public record ProbesResponseDTO(List<ProbeResponseDTO> probes) {
 }
